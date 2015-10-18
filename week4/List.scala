89383ddb0e54d792d79a61513544d91f38d58647
Matteo Bovetti <matteobovetti@gmail.com>
2015-10-18 13:15:28 +0200

    week4

--- week4/List.scala
+++ week4/List.scala
@@ -30,3 +30,9 @@ class Nil[T] extends List[T] {
   * singletone[Bolean](true)
   * 
   */
+
+object List {
+  // List(1, 2) = List.apply(1, 2)
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+  def apply[T]() = new Nil
+}
